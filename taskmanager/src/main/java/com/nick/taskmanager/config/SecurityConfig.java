862576f3198b31ed47bdc4cd18f0fef27229d49c
KirkYagami <er.nikhilsharma7@gmail.com>
2026-03-12 01:32:55 +0530

    update security config, backend ip instead of * in allowed origins

--- taskmanager/src/main/java/com/nick/taskmanager/config/SecurityConfig.java
+++ taskmanager/src/main/java/com/nick/taskmanager/config/SecurityConfig.java
@@ -82,7 +82,10 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("*"));
+            configuration.setAllowedOrigins(List.of(
+                "http://16.170.164.70",
+                "http://localhost:4200"
+            ));
             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
                     "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(List.of("*"));
